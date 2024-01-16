5740ee070d57c8a63d32a1b719dd59b240fff53f
wildanvin <danvin.jim@gmail.com>
2024-01-16 08:35:41 -0500

    Comments on the contract

--- src/PuppyRaffle.sol
+++ src/PuppyRaffle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.6;
+pragma solidity ^0.7.6; // hvz look for under/overflow if there is no SafeMath.sol 
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,8 +10,8 @@ import {Base64} from "lib/base64/base64.sol";
 /// @author PuppyLoveDAO
 /// @notice This project is to enter a raffle to win a cute dog NFT. The protocol should do the following:
 /// 1. Call the `enterRaffle` function with the following parameters:
-///    1. `address[] participants`: A list of addresses that enter. You can use this to enter yourself multiple times, or yourself and a group of your friends.
-/// 2. Duplicate addresses are not allowed
+///    1. `address[] participants`: A list of addresses that enter. You can use this to enter yourself multiple times, or yourself and a group of your friends. 
+/// 2. Duplicate addresses are not allowed. // hvz if you enter yourself multiple times duplicate addresses are allowed, rigth?
 /// 3. Users are allowed to get a refund of their ticket & `value` if they call the `refund` function
 /// 4. Every X seconds, the raffle will be able to draw a winner and be minted a random puppy
 /// 5. The owner of the protocol will set a feeAddress to take a cut of the `value`, and the rest of the funds will be sent to the winner of the puppy.
@@ -22,7 +22,7 @@ contract PuppyRaffle is ERC721, Ownable {
 
     address[] public players;
     uint256 public raffleDuration;
-    uint256 public raffleStartTime;
+    uint256 public raffleStartTime; // hvz could be constant or immutable to save gas
     address public previousWinner;
 
     // We do some storage packing to save gas
@@ -63,7 +63,7 @@ contract PuppyRaffle is ERC721, Ownable {
         raffleDuration = _raffleDuration;
         raffleStartTime = block.timestamp;
 
-        rarityToUri[COMMON_RARITY] = commonImageUri;
+        rarityToUri[COMMON_RARITY] = commonImageUri; //hvz these mappings are kind of weird
         rarityToUri[RARE_RARITY] = rareImageUri;
         rarityToUri[LEGENDARY_RARITY] = legendaryImageUri;
 
@@ -76,6 +76,7 @@ contract PuppyRaffle is ERC721, Ownable {
     /// @notice they have to pay the entrance fee * the number of players
     /// @notice duplicate entrants are not allowed
     /// @param newPlayers the list of players to enter the raffle
+    // hvz would be good to check for the empty array
     function enterRaffle(address[] memory newPlayers) public payable {
         require(msg.value == entranceFee * newPlayers.length, "PuppyRaffle: Must send enough to enter raffle");
         for (uint256 i = 0; i < newPlayers.length; i++) {
@@ -83,6 +84,7 @@ contract PuppyRaffle is ERC721, Ownable {
         }
 
         // Check for duplicates
+        // hvz it is checking only for continuous duplicates
         for (uint256 i = 0; i < players.length - 1; i++) {
             for (uint256 j = i + 1; j < players.length; j++) {
                 require(players[i] != players[j], "PuppyRaffle: Duplicate player");
@@ -93,12 +95,19 @@ contract PuppyRaffle is ERC721, Ownable {
 
     /// @param playerIndex the index of the player to refund. You can find it externally by calling `getActivePlayerIndex`
     /// @dev This function will allow there to be blank spots in the array
+    // hvz if there is a blank spot and it wins the zero address will get the ether and it wil
+    // hvz get stuck
     function refund(uint256 playerIndex) public {
         address playerAddress = players[playerIndex];
+
+        // hvz what if you pass a value outside of the array?
+        // hvz i think solidity will scream at you
         require(playerAddress == msg.sender, "PuppyRaffle: Only the player can refund");
         require(playerAddress != address(0), "PuppyRaffle: Player already refunded, or is not active");
 
         payable(msg.sender).sendValue(entranceFee);
+        // hvz good ol reentracy here. Check, Effects, Interactions please
+        // hvz also would be better pull over push when claiming eth
 
         players[playerIndex] = address(0);
         emit RaffleRefunded(playerAddress);
@@ -107,6 +116,8 @@ contract PuppyRaffle is ERC721, Ownable {
     /// @notice a way to get the index in the array
     /// @param player the address of a player in the raffle
     /// @return the index of the player in the array, if they are not active, it returns 0
+
+    //hvz how this could be a denial of service attack? i can't see
     function getActivePlayerIndex(address player) external view returns (uint256) {
         for (uint256 i = 0; i < players.length; i++) {
             if (players[i] == player) {
@@ -118,13 +129,17 @@ contract PuppyRaffle is ERC721, Ownable {
 
     /// @notice this function will select a winner and mint a puppy
     /// @notice there must be at least 4 players, and the duration has occurred
+    // hvz the four players is not enforced when creating the code
+     
     /// @notice the previous winner is stored in the previousWinner variable
     /// @dev we use a hash of on-chain data to generate the random numbers
+    // hvz weak random generation 
     /// @dev we reset the active players array after the winner is selected
     /// @dev we send 80% of the funds to the winner, the other 20% goes to the feeAddress
     function selectWinner() external {
         require(block.timestamp >= raffleStartTime + raffleDuration, "PuppyRaffle: Raffle not over");
         require(players.length >= 4, "PuppyRaffle: Need at least 4 players");
+        // hvz there could be a DoS if you create a raffle with less than 4 players
         uint256 winnerIndex =
             uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.difficulty))) % players.length;
         address winner = players[winnerIndex];
@@ -137,6 +152,7 @@ contract PuppyRaffle is ERC721, Ownable {
 
         // We use a different RNG calculate from the winnerIndex to determine rarity
         uint256 rarity = uint256(keccak256(abi.encodePacked(msg.sender, block.difficulty))) % 100;
+        // hvz block difficulty isn't now a constant?
         if (rarity <= COMMON_RARITY) {
             tokenIdToRarity[tokenId] = COMMON_RARITY;
         } else if (rarity <= COMMON_RARITY + RARE_RARITY) {
@@ -146,11 +162,15 @@ contract PuppyRaffle is ERC721, Ownable {
         }
 
         delete players;
+        // hvz does this deletes all etries of the array? or how exactly it is deleted?
         raffleStartTime = block.timestamp;
         previousWinner = winner;
         (bool success,) = winner.call{value: prizePool}("");
+        // hvz would be better a pull over push approach otherwise a DoS could happen
         require(success, "PuppyRaffle: Failed to send prize pool to winner");
         _safeMint(winner, tokenId);
+        // hvz _safeMint makes sure that the smart contract can handle the nft
+        // hvz Though it could be used as a reentrancy
     }
 
     /// @notice this function will withdraw the fees to the feeAddress
@@ -170,6 +190,7 @@ contract PuppyRaffle is ERC721, Ownable {
     }
 
     /// @notice this function will return true if the msg.sender is an active player
+    // hvz this function should be external?
     function _isActivePlayer() internal view returns (bool) {
         for (uint256 i = 0; i < players.length; i++) {
             if (players[i] == msg.sender) {
@@ -180,6 +201,7 @@ contract PuppyRaffle is ERC721, Ownable {
     }
 
     /// @notice this could be a constant variable
+    // hvz is it better to make it a constant to save gas?
     function _baseURI() internal pure returns (string memory) {
         return "data:application/json;base64,";
     }
